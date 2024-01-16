80a1ad4a462e59680056557949854e27d198b86d
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2024-01-16 12:28:13 +0100

    completed ui setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostsComponent } from './posts/posts.component';
+import { SinglePostComponent } from './single-post/single-post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, PostsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    PostsComponent,
+    SinglePostComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
